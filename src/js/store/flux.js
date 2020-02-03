58b4cf37f838571a78eb0ff5dbce9b3eda5ef9e4
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-02-03 03:49:48 +0000

    tried to fetch, didn't work

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,22 @@
-const getState = ({ getStore, setStore }) => {
+const getState = ({ getStore, setStore, getActions }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
+			loadData: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda")
+					.then(function(response) {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => setStore({ contacts: data.results }))
+					.catch(function(error) {
+						console.log("Looks like there was a problem: \n", error);
+					});
+			}
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
